1b8e2cad57ec95fb81c5a2174abdb69198d07863
Jose Miguel Bataller <jose@pragmati.co>
2017-06-16 17:07:56 +0100

    Don't delete message if not delivered to dest queue successfully

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sqs-copy
 Copy messages from AWS SQS queue to another AWS SQS
+
+
+### Install sqs-copy tool
+
+> npm install -g sqs-copy
+> sqs-copy --help
--- index.js
+++ index.js
@@ -13,63 +13,64 @@ program
   .arguments('<origin> <destination>')
   .action(function(origin, destination) {
     console.log('origin: %s destination: %s', origin, destination);
-
-      var params = {
-         QueueUrl: origin,
-         MaxNumberOfMessages: 1, // how many messages do we wanna retrieve?
-         VisibilityTimeout: 60, // seconds - how long we want a lock on this job
-         WaitTimeSeconds: 3 // seconds - how long should we wait for a message?
-       }
-
-      sqs.receiveMessage(params, function(err, data) {
-
-        if(err) { console.log("Receive error", err); throw err; }
-
-       // If there are any messages to get
-       if ((data !== undefined) && (data !== null) && data.Messages) {
-            // Get the first message (should be the only one since we said to only get one above)
-            var message = data.Messages[0];
-            //body = JSON.parse(message.Body);
-            // Now this is where you'd do something with this message
-            //doSomethingCool(body, message);  // whatever you wanna do
-            console.log("message: %s", message.Body)
-
-            sendMessage(destination, message.Body);
-
-            // Clean up after yourself... delete this message from the queue, so it's not executed again
-            removeFromQueue(origin, message);  // We'll do this in a second
-         }
-       });
-
-
+    copyNextMessage(origin, destination);
   })
   .parse(process.argv);
 
+
+/**
+ *  Copy mesage from one queue to another
+ */
+function copyNextMessage(originQueueUrl, destinationQueueUrl) {
+
+  var params = {
+     QueueUrl: originQueueUrl,
+     MaxNumberOfMessages: 1, // how many messages do we wanna retrieve?
+     VisibilityTimeout: 10, // seconds - how long we want a lock on this job
+     WaitTimeSeconds: 3, // seconds - how long should we wait for a message?
+     AttributeNames: [ "All" ],
+     MessageAttributeNames: [ "All" ]
+   }
+
+   sqs.receiveMessage(params, function(err, data) {
+
+     if(err) { console.log("Receive message error", err); throw err; }
+
+    // If there are any messages to get
+    if ((data !== undefined) && (data !== null) && data.Messages) {
+         // get first message (should be the only one since we said to only get one above)
+         var message = data.Messages[0];
+         console.log("message: %s received", message.MessageId)
+         //console.log(JSON.stringify(message))
+
+         // send message to destination queue
+         sendMessage(destinationQueueUrl, message, originQueueUrl, removeFromQueue);
+
+         copyNextMessage(originQueueUrl, destinationQueueUrl)
+      }
+    });
+}
 
 
 /**
  *  Send message to a queue
  */
-var sendMessage = function(queueUrl, body) {
+var sendMessage = function(queueUrl, message, originQueueUrl, callback) {
 
   var params = {
     DelaySeconds: 10,
-    MessageAttributes: {
-      "cid": {
-        DataType: "String",
-        StringValue: "test"
-      }
-    },
-    MessageBody: body,
+    //MessageAttributes: message.MessageAttributes,
+    MessageBody: message.Body,
     QueueUrl: queueUrl
   };
 
   sqs.sendMessage(params, function(err, data) {
     if (err) {
-      console.log("Error", err);
-      throw err;
+      console.log("Send message error", err);
+      callback(err, null, null)
     } else {
-      console.log("Success", data.MessageId);
+      console.log("message %s sent", data.MessageId);
+      callback(null, originQueueUrl, message)
     }
   });
 }
@@ -77,12 +78,15 @@ var sendMessage = function(queueUrl, body) {
 /**
  *  Remove message from queue after reading it
  */
-var removeFromQueue = function(queueUrl, message) {
+var removeFromQueue = function(err, queueUrl, message) {
+
+   if(err) throw err;
+
    sqs.deleteMessage({
       QueueUrl: queueUrl,
       ReceiptHandle: message.ReceiptHandle
    }, function(err, data) {
-      // If we errored, tell us that we did
+      // Show error message if error
       err && console.log(err);
    });
 };
